e242617480025fe5259a411286fdae881fee9179
IgorBulakh <bulach.igor@gmail.com>
2018-12-24 22:27:15 +0200

    modal_one

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,13 +4,17 @@ export default class Modal extends Component {
   state = {};
 
   render() {
+    const { onClose } = this.props;
     return (
       <div className="Backdrop">
-        <div>
+        <div className="ModalWindow">
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
             nemo recusandae sequi rerum, nesciunt corporis quisquam animi
           </p>
+          <button type="button" onClick={onClose}>
+            Close
+          </button>
         </div>
       </div>
     );
